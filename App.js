1fc10ba4442cced013a19d770afa6c36283d21ba
lydia-ain <156022235+lydia-ain@users.noreply.github.com>
2025-03-29 01:43:03 +0100

    Update App.js

--- App.js
+++ App.js
@@ -1,23 +1,31 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"; 
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom"; 
 import SignupForm from "./signupform"; 
 import LoginForm from "./loginform"; 
+import HomePage from "./Homepage";
 
 function App() {
   return (
     <Router>
       <Routes>
-        {/* Route vers la page d'inscription */}
-        <Route path="/signupform" element={<SignupForm/>} />
+        {/* Route par défaut vers la page d'accueil */}
+        <Route path="/" element={<Navigate to="/HomePage" />} />
+
+        {/* Route vers la page d'accueil */}
+        <Route path="/HomePage" element={<HomePage />} />
 
         {/* Route vers la page de connexion */}
         <Route path="/loginform" element={<LoginForm />} />
 
-        {/* Redirection par défaut vers l'inscription */}
-        <Route path="*" element={<SignupForm />} />
+        {/* Route vers la page d'inscription */}
+        <Route path="/signupform" element={<SignupForm />} />
+
+        {/* Redirection pour toute autre route */}
+        <Route path="*" element={<Navigate to="/HomePage" />} />
       </Routes>
     </Router>
   );
 }
 
 export default App;
+
